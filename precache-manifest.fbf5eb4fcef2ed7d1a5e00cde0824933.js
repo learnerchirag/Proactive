a149cb66c5a6191d362e1a147d2f52f5679902ef
chirag agrawal <cheeruagrawal@gmail.com>
2020-06-16 02:50:51 +0530

    Updates

---
+++ precache-manifest.fbf5eb4fcef2ed7d1a5e00cde0824933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ba5d86a6c61bbcf2ce6cb1681419ee",
+    "url": "/Proactive/index.html"
+  },
+  {
+    "revision": "a3282d07f6e28cec05e9",
+    "url": "/Proactive/static/css/2.ad56087f.chunk.css"
+  },
+  {
+    "revision": "b7c58914e942ba946e4b",
+    "url": "/Proactive/static/css/main.1164246b.chunk.css"
+  },
+  {
+    "revision": "a3282d07f6e28cec05e9",
+    "url": "/Proactive/static/js/2.165c5111.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Proactive/static/js/2.165c5111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c58914e942ba946e4b",
+    "url": "/Proactive/static/js/main.d2b4102f.chunk.js"
+  },
+  {
+    "revision": "54ed3c7d190ff52c9e91",
+    "url": "/Proactive/static/js/runtime-main.22496974.js"
+  }
+]);

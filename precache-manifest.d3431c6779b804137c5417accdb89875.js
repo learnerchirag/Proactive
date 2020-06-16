5e6caafc39c65071905054549b265743703d98b9
chirag agrawal <cheeruagrawal@gmail.com>
2020-06-16 20:30:19 +0530

    Updates

---
+++ precache-manifest.d3431c6779b804137c5417accdb89875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eea2a81efe8f3977432ac6a3dcc1d4e",
+    "url": "/Proactive/index.html"
+  },
+  {
+    "revision": "ba9c8914c9ce30c4a5ba",
+    "url": "/Proactive/static/css/2.68b68acb.chunk.css"
+  },
+  {
+    "revision": "162833c7803c74f5e6f3",
+    "url": "/Proactive/static/css/main.1164246b.chunk.css"
+  },
+  {
+    "revision": "ba9c8914c9ce30c4a5ba",
+    "url": "/Proactive/static/js/2.c95b1ab6.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Proactive/static/js/2.c95b1ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162833c7803c74f5e6f3",
+    "url": "/Proactive/static/js/main.d0ee3dfe.chunk.js"
+  },
+  {
+    "revision": "54ed3c7d190ff52c9e91",
+    "url": "/Proactive/static/js/runtime-main.22496974.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Proactive/precache-manifest.dbfd1b70e1a93a2f33fa696cf3ef704d.js"
+  "/Proactive/precache-manifest.d3431c6779b804137c5417accdb89875.js"
 );
 
 self.addEventListener('message', (event) => {
